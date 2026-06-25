2cb9582acfed597092266cad3deecda5ac03262e
KrishnA <krisdevd.54@gmail.com>
2026-06-25 19:18:13 +0530

    will change llm.py

--- llm.py
+++ llm.py
@@ -14,7 +14,7 @@ def rank(titles_and_urls,query):
     completion = client.chat.completions.create(
         model = "openrouter/owl-alpha",
         messages = [
-            {"role":"system","content":{system_prompt}},
+            {"role":"system","content":system_prompt},
             {"role":"user","content":f"here is the query:{query} and here is the bunch of results = {titles_and_urls}.\n give the page that will most likely answer the query."}
         ]
     )
@@ -31,14 +31,19 @@ def niche(query):
      Niche information just means it needs more searching or more results have to be analyzed.
      Your job is to make sure that response is as accurate as possible. just remember if you dont know it, its probably niche.
      just return true or false, nothing else.
+     return true if its a niche query
+     return false if its not.
+     and also provide a one line justification.
+     so required format is:
+     ""true/false"+{reason}"
     """
 
-    completions = client.chat.completions[0].create(
-        model = "openrouter/owl-alpha"
+    completions = client.chat.completions.create(
+        model = "openrouter/owl-alpha",
         messages = [
             {"role":"system","content":system_prompt},
-            {"role":"user","content":f"here's a query, you just have to classify whether the query is niche or not niche."},            
+            {"role":"user","content":f"here's a query:{query},\n you just have to classify whether the query is niche or not niche and also provide one line justififcation"},            
         ]
     )
-    content = completion.choices[0].message.content
+    content = completions.choices[0].message.content
     return content
