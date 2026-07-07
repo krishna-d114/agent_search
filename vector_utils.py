807762e7d11e1b4a649f5603c63c0ac5411bbf54
KrishnA <krisdevd.54@gmail.com>
2026-07-07 16:48:25 +0530

    about to start retrieval function still working on its arch.

---
+++ vector_utils.py
@@ -0,0 +1,29 @@
+import os 
+from dotenv import load_dotenv
+from sentece_transformers import SentenceTransformer
+from pinecone import Pinecone
+
+load_dotenv()
+
+class VectorDB:
+    def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        api_key = os.environ.get("PINECONE_API_KEY")
+        pc = Pinecone(api_key = api_key)
+        self.index = pc.Index("perplexity")
+
+    def insert_batch(self, chunks:list,url:str,title:str,doc_id:str):
+        """ insertion of all the chunks """
+        vectors = []
+        for idx,chunk_text in enumerate(chunks):
+            embedding = self.model.encode(chunk_text).tolist()
+            vectors.append((f"{doc_id}_{idx}",embedding,{
+                'chunk_text' : chunk_text,
+                'url':url,
+                'title':title
+            }))
+        
+        if vectors:
+            self.index.upsert(vectors = vectors)
+
+    
